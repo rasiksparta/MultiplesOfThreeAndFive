075187a84a56e81fba3bde3895e8c27ab934c70f
Rasik Rana <rrana@spartaglobal.com>
2019-10-04 15:22:19 +0100

    version 1.1 added skeletal test cases

---
+++ MultipleThreeAndFive/MultipleThreeAndFive.Test/MultipleThreeAndFiveTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MultipleThreeAndFive.Test
+{
+    [TestFixture]
+    class MultipleThreeAndFiveTest
+    {
+        [Test]
+        public void TestIsMultipleOfThreeOrFive()
+        {
+
+        }
+
+        [Test]
+        public void TestRangeHasNegativeBound()
+        {
+
+        }
+
+        [Test]
+        public void TestRangeIsANullSet()
+        {
+
+        }
+
+        [Test]
+        public void TestSummationOfNumberList()
+        {
+
+        }
+    }
+}
