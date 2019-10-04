de7573e23c18d1ac2bbcad0343e09a6b120b3969
Rasik Rana <rrana@spartaglobal.com>
2019-10-04 14:52:09 +0100

    version 1.0 project innitiated with MultipleThreeAndFive.Test

---
+++ MultipleThreeAndFive/MultipleThreeAndFive.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MultipleThreeAndFive.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("MultipleThreeAndFive.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5f54589-adcb-4809-a73d-6cdd1cc742f8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
